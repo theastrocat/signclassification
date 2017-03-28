0ef148e3a6a06090d05630f3b209ba3792734c00
Ubuntu <ubuntu@ip-172-31-29-93.us-west-2.compute.internal>
2017-03-28 16:57:36 +0000

    Changed port that is listened to.

--- webapp/app.py
+++ webapp/app.py
@@ -94,4 +94,4 @@ def probplot(indx):
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True)
